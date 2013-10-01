b23e12327c9109e80a1f6e9b6cda96893ba295cf
takuya513 <qwerft513@yahoo.co.jp>
2013-10-01 13:58:02 +0900

    Update QuickMergeSort.java

--- QuickMergeSort.java
+++ QuickMergeSort.java
@@ -18,106 +18,70 @@ public class QuickMergeSort<E extends Comparable> extends QuickSort<E> {
 	int threadsNum;
 
 	public QuickMergeSort(){
-		//threadsNum = Runtime.getRuntime().availableProcessors();
-		threadsNum = 8;
+		threadsNum = Runtime.getRuntime().availableProcessors();
+		//threadsNum = 2;
 		executor = Executors.newCachedThreadPool();
 
 	}
 
-	public void p(String st,int i,String st2 ,int j){
-		System.out.print("            "+st+" : "+i);
-		System.out.println("      "+st2+" : "+j);
-	}
-	public void p(String st,int i){
-		System.out.println("            "+st+" : "+i);
-	}
-
-	public void p(String st){
-		System.out.println("            "+st+":");
-	}
-
 	public void sort(E[] array){
 		this.array = array;
-		int k = array.length / threadsNum,pos_1 = 0, pos = 0,pos2 = k - 1;
-		int lastLengthOfRestSection = -1;  //-1というのは初期値として設定している
-		int specialPos = 0;
+		int arrayLength = array.length;
+		int sectionOfSort = array.length / threadsNum,middlePosOfEnd = 0, pos = 0,pos2 = sectionOfSort - 1;
+		int lastLengthOfRestSection = -1;  //繰り返し防止を確認するための変数
+		int pivotOfRest = 0; //マージするときに余った部分の仕切り
 		final List<Callable<Object>> workers = new ArrayList<Callable<Object>>(threadsNum);
-		while(pos2 < array.length){
+		
+		while(pos2 < arrayLength){
 			workers.add(Executors.callable(new QuickSortWorker(pos,pos2)));
 			pos = pos2 + 1;
-			pos2 =  k + pos2;
+			pos2 =  sectionOfSort + pos2;
 		}
 
 		//最後の区分だけ特別に処理する
-		//p("pos",pos,"k",k);
-		workers.add(Executors.callable(new QuickSortWorker(pos,array.length-1)));
-		specialPos = pos;
+		workers.add(Executors.callable(new QuickSortWorker(pos,arrayLength-1)));
+		pivotOfRest = pos;
+
 		//ここからマージ処理を行う
 		try {
 			executor.invokeAll(workers);
 
-			int i = 2;
+			int expansionSection = 2;
 			while(true){
 				workers.clear();
-				pos = 0; pos2 = k * i - 1 ;
+				pos = 0;pos2 = sectionOfSort * expansionSection - 1 ;
 
-				if(pos2 > array.length - 1){
-					//p("sard if");
+				//最後のソートの部分
+				if(pos2 > arrayLength - 1){
 					executor.shutdown();
-					//System.out.print("pos :"+0);
-					//p("pos_1",pos_1,"pos2",(array.length - 1));
-					//MyArrayUtil.print(array);
-					merge(0,pos_1-1,array.length - 1);
-					//p("after");
-					//MyArrayUtil.print(array);
+					merge(0,middlePosOfEnd-1,arrayLength - 1,new LinkedList<E>());
 					break;
 				}
 
 				while(true){
-					//System.out.println("************ "+(i*k)+"************");
-					//System.out.println("pos : "+pos+"  pos2 : "+pos2);
-					//MyArrayUtil.print(array);
 					workers.add(Executors.callable(new MergeSortWorker(pos,pos2)));
-					if(pos2 == array.length-1) {
-						//p("first if");
-						break;
-					}
+					if(pos2 == arrayLength-1) break;
+
 					pos = pos2 + 1;
-					pos_1 = pos;
-					//p("before pos2",pos2);
-					//p("before pos2",pos2,"i",i);
+					middlePosOfEnd = pos;
+					pos2 = pos2 + sectionOfSort*expansionSection;
 
-					pos2 = pos2 + k*i;
-					//p("after pos2",pos2);
+					//あまった所のソート部分
+					if(pos2 > arrayLength-1){
+						if((arrayLength - pos) == lastLengthOfRestSection)	break;  //注 繰り返しソートの防止,確認済み
 
+						if(pivotOfRest == 0)  //最初のあまり分のとき
+							workers.add(Executors.callable(new MergeSortWorker(pos,arrayLength - 1)));
+						else
+							workers.add(Executors.callable(new MergeSortWorker(pos,pivotOfRest-1,arrayLength - 1)));
 
-//					p("(array.length - pos)",(array.length - pos),"k * (i / 2))",k * (i / 2));
-					if(pos2 >= array.length){
-						if((array.length - pos) == lastLengthOfRestSection)	{
-							//p("second if");
-							break;  //注 繰り返しソートの防止,確認済み
-						}
-
-
-						//p("pos_2",pos_2,"pos",pos);
-						if(specialPos == 0){  //最初のあまり分のとき
-						//if(i == 2){
-							//p("first plex");
-							workers.add(Executors.callable(new MergeSortWorker(pos,array.length - 1)));
-							specialPos = pos;
-						}else{
-							//p("i > 2 plex");
-						workers.add(Executors.callable(new MergeSortWorker(pos,specialPos-1,array.length - 1)));
-						specialPos = pos;
-						}
-						lastLengthOfRestSection = array.length - pos;
+						pivotOfRest = pos;
+						lastLengthOfRestSection = arrayLength - pos;
 						break;
 					}
 				}
-				i = i * 2;
+				expansionSection = expansionSection * 2;
 				executor.invokeAll(workers);
-
-
 			}
 
 		} catch (InterruptedException e) {
@@ -143,34 +107,14 @@ public class QuickMergeSort<E extends Comparable> extends QuickSort<E> {
 			buff = new LinkedList<E>();
 		}
 		public void run(){
-			int i = left,j = mid + 1;
-
-			while(i <= mid && j <= right) {
-//				MyArrayUtil.print(array, i);
-//				MyArrayUtil.print(array, j);
-
-				if(array[i].compareTo(array[j]) < 0){
-					buff.add(array[i]); i++;
-				}else{
-					buff.add(array[j]); j++;
-				}
-			}
-
-			while(i <= mid) { buff.add(array[i]); i++;}
-			while(j <= right) { buff.add(array[j]); j++;}
-			for(i = left;i <= right; i++){array[i] = buff.remove(0);}
-
+			merge(left,mid,right,buff);
 		}
 	}
 
-	public void merge(int left,int mid,int right){
-		//buff.clear();  //buff用のリストを初期化しておく.修正
-		LinkedList<E> buff = new LinkedList<E>();
+	public synchronized void merge(int left,int mid,int right,LinkedList<E> buff){
 		int i = left,j = mid + 1;
 
 		while(i <= mid && j <= right) {
-//			MyArrayUtil.print(array, i);
-//			MyArrayUtil.print(array, j);
 			if(array[i].compareTo(array[j]) < 0){
 				buff.add(array[i]); i++;
 			}else{
@@ -197,5 +141,18 @@ public class QuickMergeSort<E extends Comparable> extends QuickSort<E> {
 
 	public synchronized void quickSort(int left,int right){
 		super.quickSort(left, right);
+	}
+
+
+	public void p(String st,int i,String st2 ,int j){
+		System.out.print("            "+st+" : "+i);
+		System.out.println("      "+st2+" : "+j);
+	}
+	public void p(String st,int i){
+		System.out.println("            "+st+" : "+i);
+	}
+
+	public void p(String st){
+		System.out.println("            "+st+":");
 	}
 }
