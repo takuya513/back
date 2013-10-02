d02fef20c7ea3ddd43390621ac8c6a3b77dd3fa3
takuya513 <qwerft513@yahoo.co.jp>
2013-10-02 14:54:23 +0900

    Update QuickMergeSort.java

--- QuickMergeSort.java
+++ QuickMergeSort.java
@@ -18,8 +18,8 @@ public class QuickMergeSort<E extends Comparable> extends QuickSort<E> {
 	int threadsNum;
 
 	public QuickMergeSort(){
-		//threadsNum = Runtime.getRuntime().availableProcessors();
-		threadsNum =2;
+		threadsNum = Runtime.getRuntime().availableProcessors();
+		//threadsNum =2;
 		executor = Executors.newCachedThreadPool();
 
 	}
@@ -28,8 +28,8 @@ public class QuickMergeSort<E extends Comparable> extends QuickSort<E> {
 		this.array = array;
 		int arrayLength = array.length;
 		int sectionOfSort = array.length / threadsNum,pivotOfEnd = 0, pos = 0,pos2 = sectionOfSort - 1;
-		int lastLengthOfRestSection = -1;  //繰り返し防止を確認するための変数
-		int pivotOfRest = 0; //マージするときに余った部分の仕切り
+		int lastLenOfRestSection = -1;  //繰り返し防止を確認するための変数
+		int pivotOfRest = -1; //マージするときに余った部分の仕切り
 		final List<Callable<Object>> workers = new ArrayList<Callable<Object>>(threadsNum);
 
 		//クイックソートをする
@@ -53,45 +53,49 @@ public class QuickMergeSort<E extends Comparable> extends QuickSort<E> {
 				workers.clear();
 
 				while(true){
-					
 					workers.add(Executors.callable(new MergeSortWorker(pos,pos2)));
-					if(pos2 == arrayLength-1){
-						//p("********");
+					if(pos2 == arrayLength-1)
 						break;
-					}
 
 					pos = pos2 + 1;
 					pivotOfEnd = pos;
 					pos2 = pos2 + sectionOfSort*expansionSection;
 
-					//あまった所のソート部分
 					if(pos2 > arrayLength-1){
-						if((arrayLength - pos) == lastLengthOfRestSection)	break;  //注 繰り返しソートの防止,確認済み
-
-						if(pivotOfRest == 0)  //最初のあまり分のとき
+						if(cheakSameSort(arrayLength,pos,lastLenOfRestSection)) break;
+						if(pivotOfRest == -1)  //最初のあまり分のとき
 							workers.add(Executors.callable(new MergeSortWorker(pos,arrayLength - 1)));
 						else
 							workers.add(Executors.callable(new MergeSortWorker(pos,pivotOfRest-1,arrayLength - 1)));
 
 						pivotOfRest = pos;
-						lastLengthOfRestSection = arrayLength - pos;
+						lastLenOfRestSection = arrayLength - pos;
 						break;
 					}
+
 				}
-				
-				
+
 				expansionSection = expansionSection * 2;
 				executor.invokeAll(workers);
 				pos = 0;pos2 = sectionOfSort * expansionSection - 1 ;
 			}
 			executor.shutdown();
-			//最後のソートはここで
+			//最後のmerge
 			merge(0,pivotOfEnd-1,arrayLength - 1,new LinkedList<E>());
 		} catch (InterruptedException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
 	}
+
+	//前回同じ範囲でソートしたかcheak
+	public boolean cheakSameSort(int arrayLength,int pos,int lastLenOfRestSection){
+		if((arrayLength - pos) == lastLenOfRestSection)
+			return true;
+
+		return false;
+	}
+
 
 	class MergeSortWorker implements Runnable{
 		int left,right,mid;
@@ -142,9 +146,9 @@ public class QuickMergeSort<E extends Comparable> extends QuickSort<E> {
 		}
 	}
 
-//	public  void quickSort(int left,int right){
-//		super.quickSort(left, right);
-//	}
+	//	public  void quickSort(int left,int right){
+	//		super.quickSort(left, right);
+	//	}
 
 
 	public void p(String st,int i,String st2 ,int j){
