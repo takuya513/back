aa6576a0f94a8c95fe29edb9569e6bfa1c5c50fd
takuya513 <qwerft513@yahoo.co.jp>
2013-10-04 09:39:00 +0900

    Update ParallelMergeSort.java

--- ParallelMergeSort.java
+++ ParallelMergeSort.java
@@ -1,94 +1,165 @@
-package mergeSort;
+package sort;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.atomic.AtomicInteger;
 
+import quickSort.QuickSort;
 import tools.MyArrayUtil;
+import tools.MyData;
+import tools.MyInteger;
 
-public class ParallelMergeSort<E extends Comparable> extends MergeSort<E> {
-	private final BlockingQueue<ArraysRange> tasks; //arrayの位置情報を保存しておく
-	private final AtomicInteger fixTimes;
+public class QuickMergeSort<E extends Comparable> extends QuickSort<E> {
 	ExecutorService executor;
 	int threadsNum;
 
-	public ParallelMergeSort(){
-		tasks = new LinkedBlockingQueue<ArraysRange>(Integer.MAX_VALUE);
-		fixTimes = new AtomicInteger(0);
-		threadsNum = Runtime.getRuntime().availableProcessors(); //修正
+	public QuickMergeSort(){
+		threadsNum = Runtime.getRuntime().availableProcessors();
+		//threadsNum =2;
 		executor = Executors.newCachedThreadPool();
+
 	}
 
 	public void sort(E[] array){
 		this.array = array;
-
+		int arrayLength = array.length;
+		int sectionOfSort = array.length / threadsNum,pivotOfEnd = 0, pos = 0,pos2 = sectionOfSort - 1;
+		int lastLenOfRestSection = -1;  //繰り返し防止を確認するための変数
+		int pivotOfRest = -1; //マージするときに余った部分の仕切り
 		final List<Callable<Object>> workers = new ArrayList<Callable<Object>>(threadsNum);
-		workers.add(Executors.callable(new PartitionWorker()));
-		for(int i = 1;i < threadsNum;i++)
-			workers.add(Executors.callable(new SortWorker()));
 
+		//クイックソートをする
+		while(pos2 < arrayLength){
+			workers.add(Executors.callable(new QuickSortWorker(pos,pos2)));
+			pos = pos2 + 1;
+			pos2 =  sectionOfSort + pos2;
+		}
+
+		//最後の区分だけ特別に処理する
+		workers.add(Executors.callable(new QuickSortWorker(pos,arrayLength-1)));
+		pivotOfRest = pos;
+
+		//ここからマージ処理を行う
 		try {
 			executor.invokeAll(workers);
+
+			int expansionSection = 2;
+			pos = 0;pos2 = sectionOfSort * expansionSection - 1 ;
+			while(pos2 <= arrayLength - 1){
+				workers.clear();
+
+				while(true){
+					workers.add(Executors.callable(new MergeSortWorker(pos,pos2)));
+					if(pos2 == arrayLength-1)
+						break;
+
+					pos = pos2 + 1;
+					pivotOfEnd = pos;
+					pos2 = pos2 + sectionOfSort*expansionSection;
+
+					if(pos2 > arrayLength-1){
+						if(cheakSameSort(arrayLength,pos,lastLenOfRestSection)) break;
+						if(pivotOfRest == -1)  //最初のあまり分のとき
+							workers.add(Executors.callable(new MergeSortWorker(pos,arrayLength - 1)));
+						else
+							workers.add(Executors.callable(new MergeSortWorker(pos,pivotOfRest-1,arrayLength - 1)));
+
+						pivotOfRest = pos;
+						lastLenOfRestSection = arrayLength - pos;
+						break;
+					}
+
+				}
+
+				expansionSection = expansionSection * 2;
+				executor.invokeAll(workers);
+				pos = 0;pos2 = sectionOfSort * expansionSection - 1 ;
+			}
+			executor.shutdown();
+			//最後のmerge
+			merge(0,pivotOfEnd-1,arrayLength - 1,new LinkedList<E>());
 		} catch (InterruptedException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-		executor.shutdown();
 	}
 
-	class PartitionWorker implements Runnable {
-		public void run() {
-//			System.out.println(Thread.currentThread().getName() + "  partition Start");
-			partition(0,array.length - 1);
-//			System.out.println(Thread.currentThread().getName() +    "  partition End");
+	//前回同じ範囲でソートしたかcheak
+	public boolean cheakSameSort(int arrayLength,int pos,int lastLenOfRestSection){
+		if((arrayLength - pos) == lastLenOfRestSection)
+			return true;
+
+		return false;
+	}
+
+
+	class MergeSortWorker implements Runnable{
+		int left,right,mid;
+		LinkedList<E> buff;
+		public MergeSortWorker(int left,int right){
+			this.left = left;
+			this.right = right;
+			mid = (left + right) / 2;
+			buff = new LinkedList<E>();
+		}
+
+		public MergeSortWorker(int left,int mid,int right){
+			this.left = left;
+			this.right = right;
+			this.mid = mid;
+			buff = new LinkedList<E>();
+		}
+		public void run(){
+			merge(left,mid,right,buff);
 		}
 	}
 
-	class SortWorker implements Runnable {
-		public void run() {
+	public  void merge(int left,int mid,int right,LinkedList<E> buff){
+		int i = left,j = mid + 1;
 
-			while(fixTimes.get() < array.length){
-				final ArraysRange arraysRange = tasks.poll();
-//				System.out.println(Thread.currentThread().getName()+"  left : "+arraysRange.left+"  right : "+arraysRange.right);
-//				MyArrayUtil.print(array, arraysRange.left, arraysRange.right);
-				fixTimes.incrementAndGet();
-
-				merge(arraysRange.left,arraysRange.mid,arraysRange.right);
-//				MyArrayUtil.print(array, arraysRange.left, arraysRange.right);
+		while(i <= mid && j <= right) {
+			if(array[i].compareTo(array[j]) < 0){
+				buff.add(array[i]); i++;
+			}else{
+				buff.add(array[j]); j++;
 			}
 		}
+
+		while(i <= mid) { buff.add(array[i]); i++;}
+		while(j <= right) { buff.add(array[j]); j++;}
+		for(i = left;i <= right; i++){ array[i] = buff.remove(0);}
 	}
 
-
-	public void partition(int left,int right){
-		int mid = (left + right) / 2;
-
-		if(right <= left)
-			return;
-
-		partition(left,mid);
-		partition(mid + 1, right);
-
-		tasks.offer(new ArraysRange(left,mid,right));
-	}
-
-	@Override
-	public synchronized void merge(int left,int mid,int right){
-		super.merge(left, mid, right);
-	}
-
-	static class ArraysRange {
-		final int left,mid,right;
-
-		ArraysRange(int left,int mid,int right){
+	class QuickSortWorker implements Runnable {
+		int left,right;
+		public QuickSortWorker(int left,int right){
 			this.left = left;
-			this.mid = mid;
 			this.right = right;
 		}
+
+		public void run() {
+			quickSort(left,right);
+		}
+	}
+
+	//	public  void quickSort(int left,int right){
+	//		super.quickSort(left, right);
+	//	}
+
+
+	public void p(String st,int i,String st2 ,int j){
+		System.out.print("            "+st+" : "+i);
+		System.out.println("      "+st2+" : "+j);
+	}
+	public void p(String st,int i){
+		System.out.println("            "+st+" : "+i);
+	}
+
+	public void p(String st){
+		System.out.println("            "+st+":");
 	}
 }
